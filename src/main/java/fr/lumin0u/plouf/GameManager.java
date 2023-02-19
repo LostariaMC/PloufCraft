2a6df633b9e2e63ddf5fbe2146b0d9a87c49444d
lumin0u <lumin00u@gmail.com>
2023-02-19 18:52:41 +0100

    la pioche peut se mettre en seconde main

--- src/main/java/fr/lumin0u/plouf/GameManager.java
+++ src/main/java/fr/lumin0u/plouf/GameManager.java
@@ -18,6 +18,7 @@ import net.minecraft.world.level.chunk.ChunkSection;
 import org.bukkit.*;
 import org.bukkit.Note.Tone;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -83,7 +84,9 @@ public class GameManager
 			i++;
 			
 			ploufPlayer.toBukkit().getInventory().clear();
-			ploufPlayer.toBukkit().getInventory().setItem(ploufPlayer.toCosmox().getSlotForCustomItem(PICKAXE_SLOT), PICKAXE_SLOT.getItem());
+			int pickaxeSlot = ploufPlayer.toCosmox().getSlotForCustomItem(PICKAXE_SLOT);
+			if(pickaxeSlot != -1) ploufPlayer.toBukkit().getInventory().setItem(pickaxeSlot, PICKAXE_SLOT.getItem());
+			else ploufPlayer.toBukkit().getInventory().setItem(EquipmentSlot.OFF_HAND, PICKAXE_SLOT.getItem());
 			ploufPlayer.toBukkit().addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 1234567890, 50, false, false));
 			ploufPlayer.toBukkit().setGameMode(GameMode.SURVIVAL);
 		}
