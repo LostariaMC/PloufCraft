a92ad5c832eb32bf897add77d0e025a4d1f77244
lumin0u <lumin00u@gmail.com>
2024-05-18 12:14:52 +0200

    :adhesive_bandage: players can no longer interact with blocks

--- src/main/java/fr/lumin0u/plouf/events/BlockListener.java
+++ src/main/java/fr/lumin0u/plouf/events/BlockListener.java
@@ -3,12 +3,15 @@ package fr.lumin0u.plouf.events;
 import fr.lumin0u.plouf.GameManager;
 import fr.lumin0u.plouf.Plouf;
 import fr.lumin0u.plouf.PloufPlayer;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Set;
 
 public class BlockListener implements Listener
 {
@@ -32,9 +35,11 @@ public class BlockListener implements Listener
 		PloufPlayer.of(event.getPlayer()).getPlacedBlocks().add(event.getBlock());
 	}
 	
+	private final Set<Material> forbiddenInteraction = Set.of(Material.FLOWER_POT, Material.DECORATED_POT, Material.CHISELED_BOOKSHELF, Material.ENDER_CHEST, Material.STONECUTTER);
+	
 	@EventHandler
 	public void onPlayerEditBlock(PlayerInteractEvent event) {
-		if(event.hasBlock()) {
+		if(event.hasBlock() && forbiddenInteraction.contains(event.getClickedBlock().getType())) {
 			event.setCancelled(true);
 		}
 	}
